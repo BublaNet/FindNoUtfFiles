edb18205166788a82915e6fc540e74e83ef3882d
Martin Novotný <martin.novotny@nexum.cz>
2021-04-30 03:02:23 +0200

    Add project files.

---
+++ FindNoUtfFiles/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace FindNoUtfFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.treeView1.ItemHeight = 22;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(847, 839);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 839);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ FindNoUtfFiles/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindNoUtfFiles
+{
+    public partial class Form1 : Form
+    {
+        private DirectoryInfo root { get; set; }
+        private TreeNode selectedNode { get; set; }
+        private string editor { get; set; }
+        private string[] ignoredFileTypes { get; set; }
+        private int NumberOfBadFiles { get; set; }
+        private int NumberOfFiles { get; set; }
+
+        public Form1(string root)
+        {
+            InitializeComponent();
+            this.root = new DirectoryInfo(root);
+            editor = ConfigurationManager.AppSettings["TextEditor"];
+            ignoredFileTypes = ConfigurationManager.AppSettings["IgnoredFileTypes"].ToLower().Split(new char[] { ',', ';', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            NumberOfFiles = 0;
+            NumberOfBadFiles = 0;
+            ScanDir(root, null);
+            treeView1.ExpandAll();
+            treeView1.SelectedNode = treeView1.Nodes[0];
+
+            if (NumberOfBadFiles > 0)
+                this.Text = string.Format("Nalezeno {0} souborů které nejsou v UTF-8", NumberOfBadFiles);
+            else
+                this.Text = "Všechny soubory jsou v UTF-8";
+        }
+
+        private void ScanDir(DirectoryInfo dir, TreeNode parentnode)
+        {
+            TreeNode node = new TreeNode(dir.Name);
+            node.ForeColor = Color.Black;
+            if (parentnode == null)
+            {
+                node.Text = dir.FullName;
+                treeView1.Nodes.Add(node);
+            }
+            else
+            {
+                parentnode.Nodes.Add(node);
+            }
+
+            var subdirs = Directory.GetDirectories(dir.FullName);
+            foreach (string subdir in subdirs)
+            {
+                ScanDir(new DirectoryInfo(subdir), node);
+            }
+
+            var files = Directory.GetFiles(dir.FullName);
+            foreach (string file in files)
+            {
+                if (SkipFile(file)) continue;
+
+                TreeNode fnode = new TreeNode(new FileInfo(file).Name);
+                Encoding en = GetEncoding(file);
+                if (en == Encoding.UTF8)
+                {
+                    fnode.ForeColor = Color.DarkGray;
+                }
+                else if (en == Encoding.ASCII)
+                {
+                    fnode.ForeColor = Color.Orange;
+                }
+                else
+                {
+                    fnode.ForeColor = Color.Red;
+                    NumberOfBadFiles++;
+                }
+                fnode.ToolTipText = en.ToString();
+                node.Nodes.Add(fnode);
+
+                NumberOfFiles++;
+                this.Text = string.Format("Načítám soubory...  ({0})", NumberOfFiles);
+                this.Refresh();
+            }
+
+            if (node.Nodes.Count == 0) node.Remove();
+        }
+
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) return Encoding.UTF32; //UTF-32LE
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return new UTF32Encoding(true, true);  //UTF-32BE
+
+            // We actually have no idea what the encoding is if we reach this point, so
+            // you may wish to return null instead of defaulting to ASCII
+
+            var bytes = File.ReadAllBytes(filename);
+            bool cz = bytes.Any(b => b > 127);
+            return cz ? Encoding.Default : Encoding.ASCII;
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string fileName = selectedNode.FullPath;
+
+
+            if (fileName == root.FullName)
+            {
+                Form1_Shown(this, EventArgs.Empty);
+            }
+
+            FileAttributes attr = File.GetAttributes(fileName);
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                return;
+            else
+                Process.Start(editor, fileName);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            selectedNode = e.Node;
+        }
+
+
+        private bool SkipFile(string filename)
+        {
+            foreach (string fileType in ignoredFileTypes)
+            {
+                if (filename.EndsWith("." + fileType)) return true;
+            }
+            return false;
+        }
+
+
+    }
+}
